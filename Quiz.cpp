811bac63592ba08e708c03c935d98cd96aaefd5e
brunoc1811 <bceron1811@gmail.com>
2025-06-01 17:58:09 -0700

    Update Quiz.cpp
    Ask Question

--- Quiz.cpp
+++ Quiz.cpp
@@ -71,13 +71,13 @@ const int numQues = 7; // number of questions in the quiz
 
 string funFactSpanish [numQues] =
 {
-	"¿Sabías que el francés se habla en cinco continentes?", 
-	"¡El español es el segundo idioma más hablado del mundo!", 
-	"¡El inglés se usa en la aviación y la diplomacia!", 
-	"¡En Japón, los estudiantes limpian sus salones!", 
-	"¡En Kenia, los niños aprenden en suajili e inglés!", 
-	"¡Finlandia comienza las clases tarde y aún así sobresale!", 
-	"¡India tiene más de 22 idiomas oficiales!"
+	"Â¿SabÃ­as que el francÃ©s se habla en cinco continentes?", 
+	"Â¡El espaÃ±ol es el segundo idioma mÃ¡s hablado del mundo!", 
+	"Â¡El inglÃ©s se usa en la aviaciÃ³n y la diplomacia!", 
+	"Â¡En JapÃ³n, los estudiantes limpian sus salones!", 
+	"Â¡En Kenia, los niÃ±os aprenden en suajili e inglÃ©s!", 
+	"Â¡Finlandia comienza las clases tarde y aÃºn asÃ­ sobresale!", 
+	"Â¡India tiene mÃ¡s de 22 idiomas oficiales!"
 }; 
 
 string funFactEnglish [numQues] = 
@@ -93,11 +93,43 @@ string funFactEnglish [numQues] =
 
 string funFactFrench[numQues] = 
 {
-	"Le français est parlé sur cinq continents !", 
-	"L'espagnol est la 2e langue la plus parlée au monde !", 
-	"L'anglais est utilisé dans l'aviation et la diplomatie !", 
-	"Au Japon, les élèves nettoient leurs classes chaque jour !", 
+	"Le franÃ§ais est parlÃ© sur cinq continents !", 
+	"L'espagnol est la 2e langue la plus parlÃ©e au monde !", 
+	"L'anglais est utilisÃ© dans l'aviation et la diplomatie !", 
+	"Au Japon, les Ã©lÃ¨ves nettoient leurs classes chaque jour !", 
 	"Au Kenya, les enfants apprennent le swahili et l'anglais !", 
-	"La Finlande commence l'école tard mais réussit très bien !", 
+	"La Finlande commence l'Ã©cole tard mais rÃ©ussit trÃ¨s bien !", 
 	"L'Inde a plus de 22 langues officielles !"
 }; 
+
+bool askQuestion(string language) {
+    // Step 1: Generate two random numbers
+    int num1; 3
+    int num2; 5
+
+    // Step 2: Ask the question in the selected language
+    cout << "English is used in international aviation and diplomacy!" << endl;
+    // TODO: Print question based on language
+    
+    // Step 3: Get user input
+    int userAnswer; 
+    // TODO: Use cin to get their answer
+    cin >> userAnswer;
+    // Step 4: Check if answer is correct
+    int correctAnswer; // TODO: calculate the correct answer
+
+    if (userAnswer == correctAnswer) {
+        // Step 5: Show positive feedback message
+        cout <<  "Your work is looking good!" << endl;
+        // TODO: Print a fun fact from the correct language array
+        cout << "India has more than 22 official languages!" << endl;
+
+        return true; // correct
+    } else {
+        // Step 6: Show encouragement for incorrect answer
+        cout << "No worries, keep trying!" << endl;
+        // TODO: Print "Oops! Try the next one..." in chosen language
+        cout << "Oops! Try the next one..." << endl;
+
+        return false; // incorrect
+    }
