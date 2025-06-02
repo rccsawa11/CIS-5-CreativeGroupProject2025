e6a647048f95295d3f703a2427b1f5b0b23d9246
brunoc1811 <bceron1811@gmail.com>
2025-06-01 17:19:39 -0700

    Update Quiz.cpp
    fun facts in english

--- Quiz.cpp
+++ Quiz.cpp
@@ -67,3 +67,14 @@ int main()
 	return 0;
 }
 
+const int numQues = 7; // fun facts in English
+
+string funFactEnglish[numQues] = 
+
+"Did you know? French is spoken on 5 continents!", 
+"Spanish is the 2nd most spoken language in the world!", 
+"English is used in international aviation and diplomacy!", 
+"In Japan, students clean their classrooms every day!", 
+"In Kenya, children learn in Swahili and English!", 
+"Finland starts school later but scores high!", 
+"India has more than 22 official languages!"
